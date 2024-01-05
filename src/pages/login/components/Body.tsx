5b15bd0f9cec37a68e5fd3a70d0fc99d38741285
Jonathas Marques <jonathas.souza@localiza.com>
2024-01-04 23:16:35 -0300

    feat: logica login

--- src/pages/login/components/Body.tsx
+++ src/pages/login/components/Body.tsx
@@ -1,57 +1,58 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
 
 interface Usuario {
-    nome: string;
-    senha: string;
+  nome: string;
+  senha: string;
 }
 
-interface Login { 
-    usuario: Usuario
+interface Login {
+  usuario: Usuario;
 }
 
+export async function PegaUsuario() {
+  try {
+    const response = await fetch(`http://localhost:3020/usuario/`);
 
-export async function PegaUsuario(){ 
-    try {
-        const response = await fetch(`http://localhost:3020/usuario/`)
+    if (!response.ok) {
+      throw new Error("Erro ao pegar dados");
+    }
 
-        if(!response.ok) {
-            throw new Error("Erro ao pegar dados")
-        }
-
-        const data = await response.json();
-        console.log(data)
-    } 
-    catch (error) {
-        console.error("Erro ao buscar informações da API:", error);
-        return [];
-      } 
-
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Erro ao buscar informações da API:", error);
+    return [];
+  }
 }
-
-PegaUsuario()
-
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    PegaUsuario()
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+    const usuarios = await PegaUsuario();
+
+    const email = data.get("email");
+    const password = data.get("password");
+    const usuarioEncontrado = usuarios?.find((usuario: Usuario) => {
+      return usuario.nome === email && usuario.senha === password;
     });
-    if(data.get('email') && data.get('password') )
+
+    if (usuarioEncontrado) {
+      console.log("Usuario encontrado");
+    } else {
+      console.log("Usuario não encontrado");
+    }
   };
 
   return (
@@ -61,18 +62,23 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: '#FF2700' }}>
+          <Avatar sx={{ m: 1, bgcolor: "#FF2700" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Login
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
@@ -95,10 +101,10 @@ export default function SignIn() {
             />
             <Button
               type="submit"
-              color='error'
+              color="error"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2, }}
+              sx={{ mt: 3, mb: 2 }}
             >
               Sign In
             </Button>
