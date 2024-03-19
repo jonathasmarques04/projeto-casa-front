38bddcae5e0cf12bf9ec243866a9f3afdec14a7b
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-03-19 16:26:18 -0300

    'fonte'

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <link rel="icon" href="/logo-icon.svg" />
+      <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"></link>
       <body style={{ margin: '0' }}>
         <Main />
         <NextScript />
