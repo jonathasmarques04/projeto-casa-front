a2c5285159cc2daf62394d587e6e0b6e0aca58dd
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-03-20 12:16:57 -0300

    alteracao

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AuthProvider } from "@/contexts";
 import "@/styles/globals.css";
+import theme from "@/theme/theme.mui";
+import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      </ThemeProvider>
     </AuthProvider>
     
   )
---
+++ src/theme/theme.mui.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+import { Outfit } from 'next/font/google';
+
+const outfit = Outfit({ subsets: ["latin"], weight: '500' });
+
+const theme = createTheme({ 
+    typography: {
+        fontFamily: "Outfit, sans-serif",
+    },
+});
+
+export default theme;
